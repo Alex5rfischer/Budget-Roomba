c3470171f1eb1a9bab68ed39bc44b360adf1bfe9
Alex La <alexlacentralpeelss@gmail.com>
2021-01-25 17:46:43 -0500

    Registration.java created and can toggle between the Login.java and Registration.java

--- budgetroomba/src/main/java/capstone/project/budgetroomba/Registration.java
+++ budgetroomba/src/main/java/capstone/project/budgetroomba/Registration.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
-        TextView textView = findViewById(R.id.signuptab);
+        TextView textView = findViewById(R.id.logintab);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Registration.class);
+                Intent intent = new Intent(Registration.this, Login.class);
                 startActivity(intent);
             }
         });
